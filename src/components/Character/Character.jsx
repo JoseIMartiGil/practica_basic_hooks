e1c250fec42127034c6893ad6931b4345a2255ec
JoseIMartiGil <165684119+JoseIMartiGil@users.noreply.github.com>
2024-09-29 10:12:32 +0200

    Add files via upload

---
+++ src/components/Character/Character.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./Character.css";
+const Character = ({ character }) => {
+  const [turned, setTurned] = useState(false);
+
+  return (
+    <div>
+      <img
+        className={turned ? "turned" : "image"}
+        src={character.image}
+        alt={character.name}
+        onClick={() => setTurned(!turned)}
+      />
+    </div>
+  );
+};
+
+export default Character;
